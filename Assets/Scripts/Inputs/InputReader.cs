da61e90ddff41290124b4b624c5adccb183be2ba
Alejandro Endo <lordendo96@gmail.com>
2023-03-10 18:18:39 +0100

    basic movement

---
+++ Assets/Scripts/Inputs/InputReader.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Inputs
+{
+    public class InputReader : MonoBehaviour
+    {
+        public static InputReader Instance { get; private set; }
+
+        public Vector2 Movement => _input != null ? _input.Main.Movement.ReadValue<Vector2>() : Vector2.zero;
+
+        public Vector2 Aim => _input != null ? Aiming() : Vector2.zero;
+
+        private PlayerControls _input;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            _input = new PlayerControls();
+            _input.Enable();
+        }
+
+        private Vector2 Aiming()
+        {
+            var mouseScreen = _input.Main.Aim.ReadValue<Vector2>();
+            return new Vector2(mouseScreen.x / Screen.width, mouseScreen.y / Screen.height) - Vector2.one / 2;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerComponents/Locomotion.cs
@@ -0,0 +1,37 @@
+using System;
+using Inputs;
+using UnityEngine;
+
+namespace PlayerComponents
+{
+    public class Locomotion : MonoBehaviour
+    {
+        [SerializeField] private float speed = 5f;
+        [SerializeField] private float acceleration = 10f;
+        [SerializeField] private float rotationSpeed = 100f;
+
+        private Vector3 _moveDirection;
+
+        private Rigidbody _rigidbody;
+
+        private void Awake() => _rigidbody = GetComponent<Rigidbody>();
+
+        private void Update()
+        {
+            PlayerRotation();
+            _moveDirection = new Vector3(InputReader.Instance.Movement.x, 0f, InputReader.Instance.Movement.y).normalized;
+        }
+
+        private void FixedUpdate()
+        {
+            _rigidbody.AddForce(_moveDirection * (speed * acceleration), ForceMode.Force);
+        }
+
+        private void PlayerRotation()
+        {
+            var aimDirection = new Vector3(InputReader.Instance.Aim.x, 0f, InputReader.Instance.Aim.y);
+            transform.forward =
+                Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * rotationSpeed);
+        }
+    }
+}
